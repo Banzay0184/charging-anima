06f02da73ca6b125870e31affdd1c5977bf4159d
Banzay0184 <shakhzodabidov@gmail.com>
2023-08-30 17:50:12 +0500

    0.5

--- constants/data.js
+++ constants/data.js
@@ -16,92 +16,92 @@ let data = [
 	},
 	{
 		id: 3,
-		src: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQuwyjJ1hk7W2JsJNYaui75VMCYLUMd8Ow6Rw&usqp=CAU',
+		src: 'img/4.jpeg',
 		category: 'Illustration'
 	},
 	{
 		id: 4,
-		src: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSSxfOIMMjTXnOPOQaDZxTQ5cnpfZXZzkp-2w&usqp=CAU',
+		src: 'img/5.jpeg',
 		category: 'Recommend'
 	},
 	{
 		id: 5,
-		src: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSNbPGffANbbWpPn7F4InHE_ZORjyNjWe1jYg&usqp=CAU',
+		src: 'img/6.jpeg',
 		category: 'Live'
 	},
 	{
 		id: 6,
-		src: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSUuKJVqGE2qN7rBZbedr4hd4dP8V5ls5alzQ&usqp=CAU',
+		src: 'img/7.jpeg',
 		category: 'Illustration'
 	},
 	{
 		id: 7,
-		src: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSYZgR38mQLfJ5UOZO2mZlXePZQYIXhAIT9dw&usqp=CAU',
+		src: 'img/8.jpeg',
 		category: 'Live'
 	},
 	{
 		id: 8,
-		src: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRQPP1a1Fnx2Kp6U-s4YtFi0SRkRpy_gQmxgw&usqp=CAU',
+		src: 'img/9.jpeg',
 		category: 'Recommend'
 	},
 	{
 		id: 9,
-		src: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQzXpW9YYSUxe7r4k4L1xEhsnm2cV1xXEyi5A&usqp=CAU',
+		src: 'img/10.jpeg',
 		category: 'Illustration'
 	},
 	{
 		id: 10,
-		src: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSvhAoAo9B3oMifRs-HqxwGUkNn5LHuC40AlA&usqp=CAU',
+		src: 'img/11.jpeg',
 		category: 'Live'
 	},
 	{
 		id: 11,
-		src: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRZl9DJfdvFrfjKRMczsGKo6jgINkw5xlU7oQ&usqp=CAU',
+		src: 'img/12.jpeg',
 		category: 'Recommend'
 	},
 	{
 		id: 12,
-		src: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTGExIRKicRjrHA9A0V9h_ddv7NUZqoDCmILA&usqp=CAU',
+		src: 'img/13.jpeg',
 		category: 'Illustration'
 	},
 	{
 		id: 13,
-		src: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR7jc34YtILC6GmG-isb7V-aKPW-o8G1DN-Dg&usqp=CAU',
+		src: 'img/14.jpeg',
 		category: 'Recommend'
 	},
 	{
 		id: 14,
-		src: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRN1HeMpiFqF97NnA_a3b8pVCNcW8HZ-yDuuw&usqp=CAU',
+		src: 'img/15.jpeg',
 		category: 'Live'
 	},
 	{
 		id: 15,
-		src: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT-BvhQmfD5aBfF_9V-C-8NzryhzE462yPpaA&usqp=CAU',
+		src: 'img/16.jpeg',
 		category: 'Illustration'
 	},
 	{
 		id: 16,
-		src: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTBCygwVLoXN4wn8VofMAwhENWuvDMrqSZLvQ&usqp=CAU',
+		src: 'img/17.jpeg',
 		category: 'Recommend'
 	},
 	{
 		id: 17,
-		src: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRxVy_tVB4xP3ycZvGaXmA3M7FYXtO8-N3sGg&usqp=CAU',
+		src: 'img/18.jpeg',
 		category: 'Live'
 	},
 	{
 		id: 18,
-		src: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS3FRBhcQkOwBW_M4ltB_A6u1U1wVgwVSAAgg&usqp=CAU',
+		src: 'img/19.jpeg',
 		category: 'Illustration'
 	},
 	{
 		id: 19,
-		src: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ-kg5cz6McFrA7BPxxDEHMAix5CV4peDVMGg&usqp=CAU',
+		src: 'img/20.jpeg',
 		category: 'Recommend'
 	},
 	{
 		id: 20,
-		src: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSeU5SW_hxBWTHY78AvCN5O1LaDoXGpY3yeZg&usqp=CAU',
+		src: 'img/21.jpeg',
 		category: 'Live'
 	},
 
